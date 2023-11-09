9f3017891fce2d0aa2d0e573e1c6085fd91247d6
Huy Dao <huydao226@gmail.com>
2023-11-09 15:44:13 +0700

    Update Text

--- src/components/VideoPage/VideoDescription/VideoDescription.jsx
+++ src/components/VideoPage/VideoDescription/VideoDescription.jsx
@@ -4,7 +4,7 @@ const VideoDescription = ({ description }) => {
   return (
     <div className="container-description">
       <h4 className="h4 video-heading">Description</h4>
-      <div className="video-description-play">{description}</div>
+      <div className="video-description-play">This is the demo for video streaming</div>
     </div>
   );
 };
